72747b4fe06e43c72bd86907ea0183231dbed801
erdalalioglu <erdalalioglu@gmail.com>
2022-04-20 03:37:11 +0200

    HomeWork7

---
+++ src/Hali.java
@@ -0,0 +1,26 @@
+public class Hali {
+    private double fiyat;
+
+    public Hali() {
+    }
+
+    public Hali(double fiyat) {
+        this.fiyat = fiyat;
+    }
+
+    public double getFiyat() {
+        return fiyat;
+    }
+
+    public void setFiyat(double fiyat) {
+        if (fiyat>0){
+            this.fiyat = fiyat;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Birim Fiyatı:%5.2f",fiyat);
+    }
+}
